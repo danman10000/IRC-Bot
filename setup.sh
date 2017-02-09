0059e94e386170a551d18cdaa94e576df7993d50
vp <vp@vps-MacBook-Air.local>
2017-02-09 10:28:57 -1000

    update directory del

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ sudo apt-get update
 sudo apt-get -y install python-pip
 pip install futures
 sudo apt-get -y install python-bs4sudo apt-get -y install python-scapy
-rm -rf ics355
+rm -rf ~/ics355
+rm -rf ~/ics355_botnet
 mkdir ~/ics355_botnet
 cd ~/ics355_botnet
 git clone https://github.com/danman10000/IRC-Bot.git
