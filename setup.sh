185792f13012fd13cf587f3ec17ab808146c718c
danman10000 <dclark7@hawaii.edu>
2017-02-16 12:45:06 -1000

    Update setup.sh
    Fixed some chmod issues that exist running the script as sudo

--- setup.sh
+++ setup.sh
@@ -9,7 +9,11 @@ sudo apt-get -y install git
 rm -rf ~/ics355
 rm -rf ~/ics355_botnet
 mkdir ~/ics355_botnet
+cd ~
+sudo chown -R administrator:administrator *
 cd ~/ics355_botnet
 git clone https://github.com/danman10000/IRC-Bot.git
 echo "sudo python ~/ics355_botnet/IRC-Bot/src/ircbot.py" > ~/ics355_run_bot.sh
 chmod +x ~/ics355_run_bot.sh
+cd ~
+sudo chown -R administrator:administrator *
