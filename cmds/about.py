bb577f0de7809b230e2591a558abf39bb0362cbd
Dan <dclark7@hawaii.edu>
2017-02-16 21:11:19 -1000

    moved the scripts from the root directory to here

--- cmds/about.py
+++
@@ -1,9 +0,0 @@
-def about(components): # !about
-    '''Returns a string containing info about the bot'''
-    response = ''
-
-    if components['arguments'] == '!about':
-        # the user sent just the command, no garbage
-        response = 'Author: Paullik @ http://github.com/paullik'
-
-    return response
--- cmds/answer.py
+++
@@ -1,9 +0,0 @@
-def answer(components): # !answer
-    '''Returns the ultimate answer'''
-    response = ''
-
-    if components['arguments'] == '!answer':
-        # the user sent just the command, no garbage
-        response = 'The Answer to the Ultimate Question of Life, the Universe, and Everything is 42!'
-
-    return response
